436beb8a58fe7bac0daab6ee07367ddf3da7e7b8
soso <86688573+ssofiaavila@users.noreply.github.com>
2022-04-17 23:20:05 -0300

    Update Puntaje.java

--- practica3DOO2/src/main/java/practica3DOO2/practica3DOO2/Puntaje.java
+++ practica3DOO2/src/main/java/practica3DOO2/practica3DOO2/Puntaje.java
@@ -1,5 +1,15 @@
 package practica3DOO2.practica3DOO2;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Puntaje implements Sugeridor{
 
+	@Override
+	public List<Pelicula> recomendar(List<Pelicula> vistas, List<Pelicula> disponibles) {
+		List<Pelicula> noReproducidas= disponibles.stream().filter(m -> !(vistas.contains(m))).collect(Collectors.toList());
+		return noReproducidas.stream().sorted(Comparator.comparingDouble(Pelicula:: getPuntaje).reversed()).limit(3).collect(Collectors.toList());
+	}
+	
 }
