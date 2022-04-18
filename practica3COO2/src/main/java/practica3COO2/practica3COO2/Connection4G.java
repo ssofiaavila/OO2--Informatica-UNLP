dcf4a8a411ec3a6c5f915c710d9f344b2190dfb2
soso <86688573+ssofiaavila@users.noreply.github.com>
2022-04-17 22:40:48 -0300

    Update Connection4G.java

--- practica3COO2/src/main/java/practica3COO2/practica3COO2/Connection4G.java
+++ practica3COO2/src/main/java/practica3COO2/practica3COO2/Connection4G.java
@@ -4,10 +4,11 @@ public class Connection4G {
 	private String symb;
 	
 	public Connection4G() {
+		this.symb="4G symb";
 		
 	}
 	public String transmit(String dta, int crc) {
-		return dta;
+		return "Transmitiendo data";
 	}
 	public String symb() {
 		return symb;
