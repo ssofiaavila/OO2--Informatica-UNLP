f606c94e67dfb5f9bbb13e2ce220661f34aa5628
soso <86688573+ssofiaavila@users.noreply.github.com>
2022-04-17 22:16:27 -0300

    Update ToDoItem.java

--- practica3AOO2/src/main/java/practica3AOO2/practica3AOO2/ToDoItem.java
+++ practica3AOO2/src/main/java/practica3AOO2/practica3AOO2/ToDoItem.java
@@ -2,17 +2,23 @@ package practica3AOO2.practica3AOO2;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ToDoItem {
 	private String name;
-	private LocalDateTime dateStart;
-	private String[] comments;
+	private LocalDateTime inicio;
+	private LocalDateTime fin;
+	private List<String> comments;
 	private State estado;
 	
 	
 	public ToDoItem ToDoItem(String name) {
 		this.name=name;
 		this.estado= new Pending();
+		this.inicio=null;
+		this.fin=null;
+		this.comments= new ArrayList<String>();
 		return this;
 	}
 	public void start() {
@@ -25,17 +31,40 @@ public class ToDoItem {
 	public void finish() {
 		estado=estado.finish();
 	}
+	
+	
+	public LocalDateTime getFinishDate() {
+		return this.fin;
+	}
+	public void setFinishDate(LocalDateTime fin) {
+		this.fin=fin;
+	}
+	
+	public LocalDateTime getStartDate() {
+		return this.inicio;
+	}
+	public void setStartDate(LocalDateTime inicio) {
+		this.inicio=inicio;
+	}
+	
 	public Duration workedTime() { //???
+		if (this.getStartDate() == null) {
+			throw new RuntimeException("Aun no ha iniciado la tarea");
+		}
+		if (this.getFinishDate()== null) {
+			throw new RuntimeException("La tarea no finalizó");
+		}
+		return Duration.between(this.getStartDate(), this.getFinishDate());
 		
 	}
 	public void addComment(String comment) {
 		if (!estado.getState().equals("Finished")) {
-			this.comments[this.comments.length]= comment;
+			this.comments.add(comment);
 		}
 		
 	}
 	
-	public String[] getComments() {
+	public List<String> getComments() {
 		return this.comments;
 	}
 	public State getState() {
