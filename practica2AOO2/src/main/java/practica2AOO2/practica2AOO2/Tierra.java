93443d00b0477d35bf4979fbd77c2b708a496918
soso <86688573+ssofiaavila@users.noreply.github.com>
2022-04-24 17:39:22 -0300

    Add files via upload

---
+++ practica2AOO2/src/main/java/practica2AOO2/practica2AOO2/Tierra.java
@@ -0,0 +1,34 @@
+package practica2AOO2.practica2AOO2;
+
+public class Tierra extends Topografia {
+	@Override
+	public double getProportion() {
+		return 0;
+	}
+	
+	@Override
+	public boolean equals(Object other)
+	{
+		if (other == null)
+        {
+            return false;
+        }
+	  
+		if (other == this)
+		{
+			return true;
+		}
+	  
+	 
+ 	  if (other instanceof Topografia == false) {
+		  return false;
+	  }
+ 	  
+ 	  Topografia t = (Topografia)other;
+ 	  if (this.getProportion() != t.getProportion()) {
+ 		  return false;
+ 	  }
+ 	  
+ 	  return true;
+	}
+}
